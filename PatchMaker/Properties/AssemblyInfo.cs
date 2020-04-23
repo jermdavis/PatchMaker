4c0f9379c02f842623fea0b943c91e17f43d5d1d
Jeremy Davis <Jeremy.Davis@bekagool.com>
2020-04-23 08:21:54 +0100

    Updated naming in AssemblyInfo.cs files

--- PatchMaker/Properties/AssemblyInfo.cs
+++ PatchMaker/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Patch")]
+[assembly: AssemblyTitle("PatchMaker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Patch")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("PatchMaker")]
+[assembly: AssemblyCopyright("Copyright © Jeremy Davis 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
