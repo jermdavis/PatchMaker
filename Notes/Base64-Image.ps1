db2427711d6f82b6fa957bce838d12b58792b3ee
Jeremy Davis <Jeremy.Davis@bekagool.com>
2020-04-25 18:59:25 +0100

    Updating help, and adding a script for converting help images to base64

---
+++ Notes/Base64-Image.ps1
@@ -0,0 +1,23 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]$file
+)
+
+$tmp = "tmp.txt";
+
+certutil.exe -encode $file $tmp | Out-Null
+
+$txt = [System.Io.File]::ReadAllLines($tmp);
+
+$text = ""
+foreach($line in $txt)
+{
+  if(!$line.StartsWith("-----"))
+  {
+    $text += $line
+  }
+}
+
+$text | Set-Clipboard
+
+Remove-Item $tmp
