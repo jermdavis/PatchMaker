362b6ecd477b6ebff4eac866125a726b80d9daee
Jeremy Davis <Jeremy.Davis@bekagool.com>
2020-04-28 20:57:03 +0100

    Add role and security namespaces to constants

--- PatchMaker/Namespaces.cs
+++ PatchMaker/Namespaces.cs
@@ -6,9 +6,13 @@ namespace PatchMaker
     {
         public static readonly string PatchUri = "http://www.sitecore.net/xmlconfig/";
         public static readonly string SetUri = "http://www.sitecore.net/xmlconfig/set/";
+        public static readonly string RoleUri = "http://www.sitecore.net/xmlconfig/role/";
+        public static readonly string SecurityUri = "http://www.sitecore.net/xmlconfig/security/";
 
         public static readonly XNamespace Patch = PatchUri;
         public static readonly XNamespace Set = SetUri;
+        public static readonly XNamespace Role = RoleUri;
+        public static readonly XNamespace Security = SecurityUri;
     }
 
 }
