5153ccb07eb47213c9dded66024da7c01eba3d28
Jeremy Davis <jermdavis@users.noreply.github.com>
2022-04-09 11:34:50 +0100

    Bug/45 error generating patch for delete (#46)
    * Creating failing test for #45 - xpath generation issues when namespaces on a node are all ignored
    * Prevent generation of "/element[]/node" in xpath because that's invalid - fix for crash in #45

---
+++ PatchMaker.Tests/TreeNodeExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatchMaker.App;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace PatchMaker.Tests
+{
+    
+    [TestClass]
+    public class TreeNodeExtensionsTests
+    {
+        [TestMethod]
+        public void TreeNodeExtensions_XpathForNamespacedNode_DoesNotIncludeEmptyIndexer()
+        {
+            var xml = XDocument.Load(@"..\..\ExampleXml\Sitecore.ContentSearch.Solr.DefaultIndexConfiguration.config");
+            var tree = new TreeView();
+
+            PatchProcessManager.MapTreeView(xml.Root, tree, null);
+
+            var node = tree.Nodes[0];
+
+            var xpath = TreeNodeExtensions.DefaultXPath(node);
+
+            // the xpath generation should never create
+            // "/root[]/node" when there are only namespaces to process
+            // that aren't relevant to the final expression
+            Assert.IsFalse(xpath.Contains("[]"));
+        }
+    }
+
+}
